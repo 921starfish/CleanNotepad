8a93740aa6a5bd721816f38e571f13cc7ec70198
921starfish <yuya.921.hoshino@gmail.com>
2022-08-30 21:39:46 +0900

    アクセス修飾子の変更

--- CleanNotepad.Entity/MemoEntity.cs
+++ CleanNotepad.Entity/MemoEntity.cs
@@ -3,17 +3,17 @@
     public class MemoEntity
     {
         // メモの最重要ビジネスルールは、文章
-        public string MemoText { get; set; }
+        public string MemoText { get; }
 
         // 空文字のメモは存在しない。これも最重要ビジネスルール
-        public bool IsNullOrEmpty(string rawString) => string.IsNullOrEmpty(rawString);
+        private static bool IsNotMemo(string rawString) => string.IsNullOrEmpty(rawString);
 
         // エンティティはふるまいを持てる
         // ふるまいを切り出して、別のクラスに分離してもよい。（ふるまいの分離のメリデメの話は、本筋からそれるので割愛。）
 
         public MemoEntity(string memoText)
         {
-            if (!IsNullOrEmpty(memoText))
+            if (!IsNotMemo(memoText))
             {
                 MemoText = memoText;
             }
